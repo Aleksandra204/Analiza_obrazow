6972d494327ba1c6a8c6ca04b996d086fd4f833c
marywinter3 <116124143+marywinter3@users.noreply.github.com>
2024-12-23 11:20:42 +0100

    Delete src/defineModel.m

--- src/defineModel.m
+++
@@ -1,6 +0,0 @@
-function layers = defineModel(numClasses)
-    net = resnet50;
-    layers = net.Layers;
-    layers(end-2) = fullyConnectedLayer(numClasses, 'Name', 'fc');
-    layers(end) = classificationLayer('Name', 'output');
-end
