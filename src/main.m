430f0bfac41c07dfb36541e18eb45f3a052daede
Kinga <kmurawska@student.agh.edu.pl>
2025-01-16 19:42:35 +0000

    Dodano UI. Zmieniono main.m aby był z nim kompatybilny

--- src/main.m
+++ src/main.m
@@ -1,33 +1,59 @@
-clc;
-clear;
-close all;
-
 % Load configuration settings
 config;
+cd ..;
 
-% Preprocess the data
+% Załadowanie plików -------------------------------------------
 [imdsTrain, imdsTest] = preprocessData(datasetPath, 0.8);
 
-% Define the model
+% Sieć neurownowa -------------------------------------------
 numClasses = numel(unique(imdsTrain.Labels));
 layers = defineModel(numClasses);
 
-% Set training options
-options = trainingOptions('adam', ...
-    'InitialLearnRate', learningRate, ...
-    'MaxEpochs', maxEpochs, ...
-    'MiniBatchSize', batchSize, ...
-    'Plots', 'training-progress', ...
-    'ValidationData', imdsTest);
+% Define the path to save the trained network
+networkFile = 'trainedNet.mat';
 
-% Train the model
-trainedNet = trainModel(imdsTrain, layers, options);
+% Check if the network already exists
+if isfile(networkFile)
+    % Load the trained network
+    loadedData = load(networkFile);
+    trainedNet = loadedData.trainedNet;
+else
+    % Set training options
+    options = trainingOptions('adam', ...
+        'InitialLearnRate', learningRate, ...
+        'MaxEpochs', maxEpochs, ...
+        'MiniBatchSize', batchSize, ...
+        'Plots', 'training-progress', ...
+        'ValidationData', imdsTest);
+
+    % Train the model
+    trainedNet = trainModel(imdsTrain, layers, options);
+    
+    % Save the trained network
+    save(networkFile, 'trainedNet');
+end
 
 % Evaluate the model
 accuracy = testModel(trainedNet, imdsTest);
 disp(['Model accuracy: ', num2str(accuracy * 100), '%']);
 
-% Predict the brand of a new car image
-newImagePath = 'loaded/test.png';
+
+
+% Przewidywanie marki -------------------------------------------
+folderPath = 'loaded';
+
+% Pobierz informacje o pliku w folderze
+files = dir(fullfile(folderPath, '*.*')); % Wyszukaj wszystkie pliki (z rozszerzeniem)
+
+% Filtruj, aby znaleźć plik (w folderze powinien być tylko jeden plik)
+imageFile = files(~[files.isdir]); % Usuń katalogi z listy
+
+if isempty(imageFile)
+    error('Brak pliku w folderze.');
+else
+    % Zmienna imageFile przechowuje dane o pliku
+    newImagePath = fullfile(imageFile(1).folder, imageFile(1).name);  % Pełna ścieżka do pliku
+end
+
 label = predictCarBrand(newImagePath, trainedNet);
-disp(['Predicted car brand: ', char(label)]);
+app.MarkaLabel.Text = char(label);
